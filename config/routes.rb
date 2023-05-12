b92f2ac8f42fe7ab765d6e604e144cb0d27df054
GeorgePires <georgeasp.pi@gmail.com>
2023-05-12 18:10:43 -0300

    :sparkles: feat: Generate a controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+  get 'tasks/new'
+  get 'tasks/edit'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'home#index'
+  match 'about', to: 'home#about', via: :get
 end
