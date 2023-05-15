ccf717d505e19d08b48a83404edc2464a51f4058
GeorgePires <georgeasp.pi@gmail.com>
2023-05-14 21:48:14 -0300

    :sparkles: feat: category

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class CategoriesController < ApplicationController
+  def index; end
+
+  def show; end
+  def new; end
+
+  def edit; end
+  def create; end
+
+  def update; end
+
+  def destroy; end
+end
