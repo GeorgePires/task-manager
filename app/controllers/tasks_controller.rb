c2c4bebdd16b80823e8a38dbdd9fc1c36cf1975f
GeorgePires <georgeasp.pi@gmail.com>
2023-05-14 21:47:38 -0300

    :sparkles: feat: add CRUD and views

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,55 @@
 # frozen_string_literal: true
 
 class TasksController < ApplicationController
-  def index; end
+  before_action :set_task, only: %i[show edit update destroy]
 
-  def new; end
+  def index
+    @tasks = Task.all
+  end
+
+  def show; end
+
+  def new
+    @count = Task.count
+    @task = Task.new(position: @count + 1)
+  end
 
   def edit; end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: 'Task was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to task_path(@task), notice: 'Task was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, notice: 'Task was successfully destroyed.'
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(
+      :name,
+      :position,
+      :completed,
+      :description
+    )
+  end
 end
