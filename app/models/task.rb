d4eef9bfde1a8c38a8dbba7007e6f32eefad4266
GeorgePires <georgeasp.pi@gmail.com>
2023-05-29 14:48:56 -0300

    :sparkles: feat: add scopes

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,11 @@ class Task < ApplicationRecord
   belongs_to :category, optional: true
 
   # normalizes :name, with: ->(name) { name.donwcase.titleize }
+
+  scope :sorted, -> { order(:position) }
+
+  scope :complete, -> { where(completed: true) }
+  scope :incomplete, -> { where(completed: false) }
+
+  scope :search, ->(word) { where('LOWER(name) LIKE ?', "%#{word.downcase}%") }
 end
