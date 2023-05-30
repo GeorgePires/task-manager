6845b2a3837a2c077dae97f8af6fcdd88375b808
GeorgePires <georgeasp.pi@gmail.com>
2023-05-29 22:42:29 -0300

    :sparkles: feat: add custom validate

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,8 +8,23 @@ class Task < ApplicationRecord
   validates :name, length: { minimum: 3, maximum: 50 }
   validates :position, numericality: { greater_than: 0 }
 
+  validate :description_has_no_prohibited_words
+
   scope :sorted, -> { order(:position) }
   scope :complete, -> { where(completed: true) }
   scope :incomplete, -> { where(completed: false) }
   scope :search, ->(word) { where('LOWER(name) LIKE ?', "%#{word.downcase}%") }
+
+  private
+
+  def description_has_no_prohibited_words
+    return unless description.present?
+
+    prohibited_words = %w[later eventually someday]
+    prohibited_words.each do |word|
+      if description.include?(word)
+        errors.add(:description, "Cannot contain prohibited word: #{word}")
+      end
+    end
+  end
 end
