be6cc9b201803686824a8905c45e1cc5efc4776c
GeorgePires <georgeasp.pi@gmail.com>
2023-05-29 23:28:15 -0300

    :sparkles: feat: add callbacks

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,6 +4,12 @@ class Task < ApplicationRecord
   belongs_to :category, optional: true
   # normalizes :name, with: ->(name) { name.donwcase.titleize }
 
+  before_validation :titleize_name, :set_default_position
+  before_create :log_create
+  before_update :log_update
+  after_save :log_save
+  after_commit :cleaning_reminder
+
   validates :name, :position, presence: true
   validates :name, length: { minimum: 3, maximum: 50 }
   validates :position, numericality: { greater_than: 0 }
@@ -22,9 +28,36 @@ class Task < ApplicationRecord
 
     prohibited_words = %w[later eventually someday]
     prohibited_words.each do |word|
-      if description.include?(word)
-        errors.add(:description, "Cannot contain prohibited word: #{word}")
-      end
+      errors.add(:description, "Cannot contain prohibited word: #{word}") if description.include?(word)
     end
+  end
+
+  def titleize_name
+    self.name = name.titleize
+  end
+
+  def set_default_position
+    return unless position.blank? || position < 1
+
+    max = Task.maximum(:position) || 0
+    self.position = max + 1
+  end
+
+  def log_create
+    logger.debug("Task being created: #{name}")
+  end
+
+  def log_update
+    logger.debug("Task being updated: #{name}")
+  end
+
+  def log_save
+    # runs on both create & update
+    logger.debug("Task was saved: #{name}")
+  end
+
+  def cleaning_reminder
+    # This could be a placeholder for sending an email to an admin
+    logger.debug('Remember to prune old tasks')
   end
 end
